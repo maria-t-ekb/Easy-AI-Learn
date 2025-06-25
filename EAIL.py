d50b6ec7543729e7de6230cf631f632670c42552
maria-t-ekb <89733775+maria-t-ekb@users.noreply.github.com>
2025-06-26 01:14:25 +0500

    Update EAIL.py

--- EAIL.py
+++ EAIL.py
@@ -10,16 +10,20 @@ app = Flask(__name__)
 # def index():
 #     return send_from_directory('.', 'index_image_all.html')
 
-@app.before_request
-def before_first_request():
-    init_db()
+# @app.before_request
+# def before_first_request():
+#     init_db()
+
+# @app.get('/favicon.ico')
+# def get_favicon():
+#     return Flask.send_static_file('static/favicon.ico')
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
+    # conn = get_db_connection()
+    # posts = conn.execute('SELECT * FROM posts').fetchall()
+    # conn.close()
+    return render_template('index.html')   # , posts=posts
 
 @app.route('/image_all_dataset')
 def image_all_dataset():
@@ -92,9 +96,8 @@ def audio_all_dataset_upload():
     y = int(request.form['epochs'])
     d = 1
 
-    # return yolo_train.y_train(f'uploads/{dir_name}', y, d)
+    return yolo_train.train_and_test('audio', 1, f'uploads/{dir_name}', epoch=y)
     # return f'{dir_name}, {y}, {d}'
-    return ' '
 
 
 @app.route('/pose_all_dataset_upload', methods=['POST'])
@@ -108,9 +111,8 @@ def pose_all_dataset_upload():
     y = int(request.form['epochs'])
     d = 1
 
-    # return yolo_train.y_train(f'uploads/{dir_name}', y, d)
+    return yolo_train.train_and_test('pose', 1, f'uploads/{dir_name}', epoch=y)
     # return f'{dir_name}, {y}, {d}'
-    return ' '
 
 
 @app.route('/text_all_dataset_upload', methods=['POST'])
@@ -124,9 +126,8 @@ def text_all_dataset_upload():
     y = int(request.form['epochs'])
     d = 1
 
-    # return yolo_train.y_train(f'uploads/{dir_name}', y, d)
+    return yolo_train.train_and_test('text', 1, f'uploads/{dir_name}', epoch=y)
     # return f'{dir_name}, {y}, {d}'
-    return ' '
 
 
 @app.route('/part_dataset_upload', methods=['POST'])
@@ -156,8 +157,7 @@ def image_part_dataset_train():
     y = int(request.form['epochs'])
     d = 1
     # return f'{project_name}, {y}, {d}'
-    # return yolo_train.y_train(f'uploads/{project_name}', y, d)
-    return
+    return yolo_train.y_train(f'uploads/{project_name}', y, d)
 
 @app.route('/audio_part_dataset_train', methods=['POST'])
 def audio_part_dataset_train():
@@ -165,8 +165,7 @@ def audio_part_dataset_train():
     y = int(request.form['epochs'])
     d = 1
     # return f'{project_name}, {y}, {d}'
-    # return yolo_train.y_train(f'uploads/{project_name}', y, d)
-    return ' '
+    return yolo_train.train_and_test('audio', 1, f'uploads/{project_name}', y, d)
 
 @app.route('/pose_part_dataset_train', methods=['POST'])
 def pose_part_dataset_train():
@@ -174,7 +173,7 @@ def pose_part_dataset_train():
     y = int(request.form['epochs'])
     d = 1
     # return f'{project_name}, {y}, {d}'
-    return yolo_train.y_train(f'uploads/{project_name}', y, d)
+    return yolo_train.train_and_test('pose', 1, f'uploads/{project_name}', epoch=y)
 
 @app.route('/text_part_dataset_train', methods=['POST'])
 def text_part_dataset_train():
@@ -182,61 +181,75 @@ def text_part_dataset_train():
     y = int(request.form['epochs'])
     d = 1
     # return f'{project_name}, {y}, {d}'
-    # return yolo_train.y_train(f'uploads/{project_name}', y, d)
-    return ' '
+    return yolo_train.train_and_test('text', 1, f'uploads/{project_name}', epoch=y)
 
 
 @app.route('/image_test', methods=['POST'])
 def image_test():
-    file = request.files['file']
-    filename = file.filename
-    file.save(f'uploads/{filename}')
-    return yolo_train.y_test(f'uploads/{filename}')
-    # return 'file'
+    try:
+        file = request.files['file']
+        filename = file.filename
+        file.save(f'uploads/{filename}')
+        return yolo_train.y_test(f'uploads/{filename}')
+        # return 'file'
+    except:
+        return ' '
 
 @app.route('/audio_test', methods=['POST'])
 def audio_test():
-    file = request.files['file']
-    filename = file.filename
-    file.save(f'uploads/{filename}')
-    # return yolo_train.y_test(f'uploads/{filename}')
-    return ' '
+    try:
+        file = request.files['file']
+        filename = file.filename
+        file.save(f'uploads/{filename}')
+        return yolo_train.train_and_test('audio', 2, f'uploads/{filename}')
+        # return 'file'
+    except:
+        return ' '
 
 @app.route('/pose_test', methods=['POST'])
 def pose_test():
-    file = request.files['file']
-    filename = file.filename
-    file.save(f'uploads/{filename}')
-    # return yolo_train.y_test(f'uploads/{filename}')
-    return ' '
+    try:
+        file = request.files['file']
+        filename = file.filename
+        file.save(f'uploads/{filename}')
+        return yolo_train.train_and_test('pose', 2, f'uploads/{filename}')
+        # return 'file'
+    except:
+        return ' '
 
 @app.route('/text_test', methods=['POST'])
 def text_test():
-    file = request.files['file']
-    filename = file.filename
-    file.save(f'uploads/{filename}')
-    # return yolo_train.y_test(f'uploads/{filename}')
-    # return 'file'
-    return ' '
+    try:
+        text = request.form['text']
+        # filename = file.filename
+        # file.save(f'uploads/{filename}')
+        return yolo_train.train_and_test('text', 2, text)
+        # return 'file'
+        # return text
+    except:
+        return ' '
 
 
 @app.route('/upload_neuro')
 def upload_neuro():
-    path = "runs/classify/train/weights/best.pt"
-    return send_file(path, as_attachment=True, download_name='neuro.pdf')
+    try:
+        path = "uploads/Без названия.jpg"
+        return send_file(path, as_attachment=True, download_name='neuro.pdf')
+    except:
+        return ' '
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+# def get_db_connection():
+#     conn = sqlite3.connect('database.db')
+#     conn.row_factory = sqlite3.Row
+#     return conn
 
-def close_db_connection(conn):
-    conn.close()
-
-def init_db():
-    conn = get_db_connection()
-    conn.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)')
-    conn.close()
+# def close_db_connection(conn):
+#     conn.close()
+#
+# def init_db():
+#     conn = get_db_connection()
+#     conn.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)')
+#     conn.close()
 
 if __name__ == '__main__':
     # app.run()
